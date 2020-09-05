8dc4ef27eff3ce3c9e42701d83c5ca5bcf2c7324
Glitch (mysteriousbot-developerlol) <none>
2020-09-05 23:18:43 +0000

    💂🏠 Updated with Glitch

---
+++ commands/info/ping.js
@@ -0,0 +1,11 @@
+const discord = require('discord.js')
+module.exports = {
+  name: "ping",
+  category: "info",
+  description: "Get bot ping :/",
+  usage: "ping",
+  run: (client, message) => {
+    message.channel.send(`**Pong** ${client.ws.ping}`);
+  }
+  
+}
